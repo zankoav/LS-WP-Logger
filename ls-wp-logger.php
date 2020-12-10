cc8a2758a18d26f8fa9983073c4d6c4fca305035
Alexandr <aleksandr.zanko@enway.com>
2020-12-10 15:05:26 +0300

    update wp issuies

--- ls-wp-logger.php
+++ ls-wp-logger.php
@@ -9,6 +9,8 @@
     License: GPL2
    */
 
+define('PLUGIN_NAME', basename( __DIR__ ));
+
 require __DIR__ . '/LS_WP_Logger.php';
 
 register_activation_hook( __FILE__, function() {
@@ -16,38 +18,39 @@ register_activation_hook( __FILE__, function() {
 });
 
 add_action( 'admin_menu', function() {
-
     add_menu_page(
         'LS WP Logger',
         'LS WP Logger',
         'manage_options',
-        'LS-WP-Logger/menu-page.php',
+        PLUGIN_NAME .'/ls-wp-logger-page.php',
         '',
-        plugins_url( 'LS-WP-Logger/images/menu-icon.svg' ),
+        plugins_url( 'images/menu-icon.svg', __FILE__ ),
         81
     );
 });
 
 add_action( 'wp_ajax_delete_logs', function () {
     LS_WP_Logger::deleteLogs();
-	$logs = LS_WP_Logger::getLogs($_POST['type']);
+	$logs = LS_WP_Logger::getLogs(sanitize_text_field($_POST['type']));
     echo json_encode($logs);
 	wp_die();
 });
 
 add_action( 'wp_ajax_get_logs', function () {
-    $logs = LS_WP_Logger::getLogs($_POST['type']);
+    $logs = LS_WP_Logger::getLogs(sanitize_text_field($_POST['type']));
     echo json_encode($logs);
 	wp_die();
 });
 
-add_action( 'admin_enqueue_scripts', function() {
-    wp_enqueue_style('ls_wp_css', plugins_url('css/style.css',__FILE__ ));
-    wp_enqueue_script('ls_wp_js', plugins_url('js/main.js',__FILE__ ));
-    wp_localize_script( 'jquery', 'lsWpAjax', 
-        array(
-            'url' => admin_url('admin-ajax.php'),
-            'logs' => json_encode(LS_WP_Logger::getLogs())
-        )
-    );  
+add_action( 'admin_enqueue_scripts', function($hook_suffix) {
+    if(is_admin() and $hook_suffix === PLUGIN_NAME.'/ls-wp-logger-page.php') {
+        wp_enqueue_style('ls_wp_css', plugins_url('css/style.css',__FILE__ ));
+        wp_enqueue_script('ls_wp_js', plugins_url('js/main.js',__FILE__ ));
+        wp_localize_script( 'jquery', 'lsWpAjax', 
+            array(
+                'url' => admin_url('admin-ajax.php'),
+                'logs' => json_encode(LS_WP_Logger::getLogs())
+            )
+        );  
+    }
 } );
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: lightningsoft
 Tags: log, logs, data logs
 Requires at least: 5.4
-Tested up to: 5.4
+Tested up to: 5.6
 Stable tag: 1.0.0
 Requires PHP: 5.6
 License: GPLv2 or later
